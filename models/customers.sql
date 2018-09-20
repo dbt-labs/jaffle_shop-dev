f783ac22d44a66f35886be97655d4ea3da2d575b
Claire Carroll <carroll.claire.e@gmail.com>
2018-09-20 18:41:47 +1000

    Build a basic model for dbt tutorials

---
+++ models/customers.sql
@@ -0,0 +1,22 @@
+with customer_orders as (
+  select
+  customer_id
+  , min(order_date) as first_order
+  , max(order_date) as most_recent_order
+  , count(id) as number_of_orders
+  , sum(amount) as total_order_amount
+  from {{ ref('raw_orders') }}
+  group by 1
+)
+
+select
+raw_customers.id
+, raw_customers.first_name
+, raw_customers.last_name
+, raw_customers.email
+, customer_orders.first_order
+, customer_orders.most_recent_order
+, customer_orders.number_of_orders
+, customer_orders.total_order_amount
+from {{ ref('raw_customers') }} as raw_customers
+left join customer_orders on raw_customers.id = customer_orders.customer_id
---
+++ models/orders.sql
@@ -0,0 +1,23 @@
+{% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
+with order_payments as (
+  select
+  order_id,
+  {% for payment_method in payment_methods %}
+  sum(case when payment_method = '{{payment_method}}' then amount else 0 end) as {{payment_method}}_amount,
+  {% endfor %}
+  sum(amount) as total_amount
+  from {{ ref('raw_payments') }}
+  group by 1
+)
+
+select
+raw_orders.id
+, raw_orders.customer_id
+, raw_orders.order_date
+, raw_orders.status
+, raw_orders.amount
+{% for payment_method in payment_methods %}
+, order_payments.{{payment_method}}_amount
+{% endfor %}
+from {{ ref('raw_orders') }} as raw_orders
+left join order_payments on order_payments.order_id = raw_orders.id
