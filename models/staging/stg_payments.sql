51d77f13c1bba3965760a50711991e3223e21a5f
dbt Labs <dev@dbtlabs.com>
2023-08-17 02:02:22 +0000

    test commit to regression test ci

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -14,7 +14,7 @@ renamed as (
         id as payment_id,
         order_id,
         payment_method,
-
+        -- add a comment to run ci again
         --`amount` is currently stored in cents, so we convert it to dollars
         amount / 100 as amount
 
