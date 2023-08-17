d691dcf3b3cacbdaf8318e7bee09c5f83e3253b5
Paul Pham <dev@dbtlabs.com>
2023-08-17 21:11:29 +0000

    test draft pr logs

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -14,7 +14,7 @@ renamed as (
         id as payment_id,
         order_id,
         payment_method,
-
+        -- add a comment
         --`amount` is currently stored in cents, so we convert it to dollars
         amount / 100 as amount
 
