06046b69e695d962c8902c1feb653de7dffea760
Ayush Shukla <61577511+ayshukla@users.noreply.github.com>
2023-08-17 18:18:56 -0700

    Update customer_orders.sql

--- models/marts/core/intermediate/customer_orders.sql
+++ models/marts/core/intermediate/customer_orders.sql
@@ -12,6 +12,8 @@ final as (
         min(order_date) as first_order,
         max(order_date) as most_recent_order,
         count(order_id) as number_of_orders
+
+    
     from orders
 
     group by 1
