d5e49ed09dd53697670927629260caf195f1da8b
Paul Pham <dev@dbtlabs.com>
2023-08-22 17:22:54 +0000

    test basic ci sync again

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -14,7 +14,7 @@ renamed as (
         id as payment_id,
         order_id,
         payment_method,
-
+        amount as raw_amount_value,
         --`amount` is currently stored in cents, so we convert it to dollars
         amount / 100 as amount
 
